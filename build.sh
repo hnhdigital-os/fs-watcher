27a5bb62aab9d5779e29f4a1b5ff6e55a0cee14f
Rocco Howard <rocco@hnh.digital>
2019-01-02 06:19:14 +0000

    Remove any builds

--- build.sh
+++ build.sh
@@ -77,6 +77,7 @@ if [ "stable" != "${MODE}" ]; then
 
   if [ ! -f "${ROOT}/${MODE_TARGET}/${VERSION}" -o "${VERSION}" != "`cat \"${ROOT}/${MODE_TARGET}/latest\"`" ]; then
     rm -rf "${ROOT}/${MODE_TARGET}/download/"
+    rm -rf "${ROOT}/${MODE_TARGET}/builds/"
     mkdir -p "${ROOT}/${MODE_TARGET}/download/${VERSION}/"
     ${COMPOSER} install -q --no-dev && \
     bin/compile ${MODE} ${VERSION} && \
